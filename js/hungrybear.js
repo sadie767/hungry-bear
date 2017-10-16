585769e2694ce15a3a6e779f7ef629057b467a57
rm <biffbangpow767@yahoo.com>
2017-10-15 22:40:21 -0700

    practice asynchrony

---
+++ js/hungrybear.js
@@ -0,0 +1,33 @@
+export class HungryBear {
+
+  constructor(name) {
+    this.name = name;
+    this.foodLevel = 10;
+  }
+
+  setHunger() {
+    setInterval(() => {
+      this.foodLevel--;
+    }, 1000);
+  }
+
+  didYouGetEaten() {
+    if (this.foodLevel > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  feed() {
+    this.foodLevel = 10;
+  }
+
+  let heyThere = welcome(“Hey there”);
+  
+  function welcome(salutation) {
+  return function(yourName) {
+    return `${salutation}! Nice to meet you, ${yourName}!`
+  }
+}
+}
